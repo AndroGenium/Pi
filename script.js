e43c6a6ac5791cf051642dc6234a265043fff2ef
Andro Chilachava <157217297+AndroGenium@users.noreply.github.com>
2024-03-01 17:18:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const RandomNumber = Math.random()*10
+console.log("The Random Number Is " + RandomNumber)
+
+const pi = 3.14159
+console.log("pi is normally " + pi + "... and more")
+
+const piRounded = Math.round(pi)
+const piFloor = Math.floor(pi)
+const piCeiling = Math.ceil(pi)
+console.log("but pi rounded is " + piRounded)
+console.log("pi rounded to the floor is " + piFloor)
+console.log("pi rounded to the ceiling is " +piCeiling)
+
+const Number = 9
+const SQRT= Math.sqrt(Number)
+console.log("Square root of " + Number +" is " + SQRT)
